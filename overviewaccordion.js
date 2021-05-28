bf9edaae5e4d6da8fdfd5f085a8d4bccd8dcebe5
MUM-MIH <83559822+MUM-MIH@users.noreply.github.com>
2021-05-28 12:04:14 +0800

    Update overviewaccordion.js

--- overviewaccordion.js
+++ overviewaccordion.js
@@ -2,7 +2,7 @@ if (/section/.test(window.location.href)) {
         document.getElementById('hiddenoverview').style.display = 'none';
       }
 
-      $(function() {
+      jQuery(function() {
         if ( $( "#accordion" ).length >0 ) {
                 $("#accordion").accordion({
                         header: "h5",

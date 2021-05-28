1143414f0ffae93bf4542877cd65576331f7c2fb
MUM-MIH <83559822+MUM-MIH@users.noreply.github.com>
2021-05-28 10:55:06 +0800

    Update overviewaccordion.js

--- overviewaccordion.js
+++ overviewaccordion.js
@@ -1,10 +1,5 @@
 // directs the function to work once the page has loaded.
 window.onload=function(){	
-	if (/section/.test(window.location.href)) {
-        document.getElementById('hiddenoverview').style.display = 'none';
-      }
-
-	if ( $( "#accordion" ).length ) {
                 $("#accordion").accordion({
                         header: "h5",
                         collapsible: false,

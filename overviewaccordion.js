e21ee4b2d17e28d708a8782ea134fe9baf22fc2a
MUM-MIH <83559822+MUM-MIH@users.noreply.github.com>
2021-05-28 10:39:22 +0800

    Update overviewaccordion.js

--- overviewaccordion.js
+++ overviewaccordion.js
@@ -1,4 +1,15 @@
-if (/section/.test(window.location.href)) {
+// directs the function to work once the page has loaded.
+window.onload=function(){
+// change all links that have #section- in them which goes to the all sections page, to &section= links that go to the single section instead
+	$('nav a, .bookexit').each(function(){
+        	this.href=this.href.replace('#section-','&section=');
+// opens the atto editor completely on load    
+    		$('.editor_atto_toolbar div').removeAttr('hidden');
+  		$('.editor_atto_toolbar div').css('display', 'inline-block');
+// opens the tinyMCE toolbar rows 2 and 3 on load (once a user collapses this again, it stops working)
+   		$('.mceToolbarRow2').css('display', 'table');
+  		$('.mceToolbarRow3').css('display', 'table');
+  	});if (/section/.test(window.location.href)) {
         document.getElementById('hiddenoverview').style.display = 'none';
       }
 
@@ -10,3 +21,4 @@ if ( $( "#accordion" ).length ) {
                         active: 0,
                 });
          }
+
